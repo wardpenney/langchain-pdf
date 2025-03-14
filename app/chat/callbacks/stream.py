313d2af4143d5c57d35a84d4362412b10a5584ea
Ward Penney <ward@wardpenney.com>
2025-03-14 11:34:14 -0600

    Isolate condensed chain and conversational chain handlers

--- app/chat/callbacks/stream.py
+++ app/chat/callbacks/stream.py
@@ -4,12 +4,19 @@ from queue import Queue
 class StreamingHandler(BaseCallbackHandler):
   def __init__(self, queue: Queue):
     self.queue = queue
+    self.streaming_run_ids = set()
+
+  def on_chat_model_start(self, serialized, messages, run_id, **kwargs):
+    if serialized['kwargs']['streaming']:
+      self.streaming_run_ids.add(run_id)
 
   def on_llm_new_token(self, token, **kwargs):
     self.queue.put(token)
 
-  def on_llm_end(self, response, **kwargs):
-    self.queue.put(None)
+  def on_llm_end(self, response, run_id, **kwargs):
+    if run_id in self.streaming_run_ids:
+      self.queue.put(None)
+      self.streaming_run_ids.remove(run_id)
 
   def on_llm_error(self, error, **kwargs):
     self.queue.put(None)
